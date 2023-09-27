0e36b44b253dc740fd6c71b914a648a3ca9621b1
Brandon <brandonblumenberg@ymail.com>
2023-09-27 19:50:12 -0400

    Completed lesson on datatypes

--- Unit_2/dataTypes.js
+++ Unit_2/dataTypes.js
@@ -51,3 +51,113 @@ console.log(numbersAreHard); // 0.3
 // Quick things:
 let a = Number("42"); // turns string of numbers into numbers. 
 console.log(a);
+
+/* 
+    String
+    - Strings are datatypes to represent text.
+    - Wrapped in single or double quotes.
+
+*/
+
+let stringOne = "double quotes";
+const stringTwo = 'single quotes';
+
+console.log(stringOne, stringTwo); //double quotes single quotes
+
+// Numbers vs. Strings
+let first = 1234 + 567;
+let second = '1234' + '567';
+
+console.log(first) // 1801, added number values together.
+console.log(second) // 1234567, placed a string after another.
+
+console.log(typeof first);
+console.log(typeof second);
+
+/* 
+    Objects
+    - Used to store many values instead of a regular one.
+    - Need to be denoted with {}.
+    - Inside the object, we use key value pairs
+        key: value <-- multiple key pairs must be separated by a comma.
+*/
+
+let frodo = {
+    race: 'hobbit',
+    altName: 'The Ring Bearer',
+    rings: 1,
+    cloak: true
+}
+
+console.log(frodo);
+console.log(typeof frodo);
+console.log(frodo.altName);
+
+/* 
+    Array
+    - Containers that hold a list(s) of items.
+    - Need to denote arrays with [Square Brackets].
+    - An array with multiple items needs those items to be comma separated.
+*/
+
+//  (1)   (2)          (3)
+let list = [ 'item1','item2', 'item3' ];
+/* 
+    1. name of the array/list.
+    2. Array is inside of [].
+    3. Each item, regardless of datatype, is comma separated.
+*/
+
+let burritos = [ 'large', 2, true ]; // [ 'large', 2, true ]
+console.log(burritos); // object
+
+console.log(typeof burritos); //object, JS defines objects as a container that can hold multiple data types.
+
+console.log(burritos[0]);
+
+/* 
+    DataType Literals:
+    - A literal represents a fixed value that we as developers insert into the code.
+
+    - Literals Include:
+        - string literals(character wrapped in quotations)
+        - numeric literals (integers)
+        - boolean literals (true/false)
+        - object literals (key/value pairs)
+        - array literals (lists)
+*/
+
+// STRING LITERAL
+let car = 'Ford';
+
+// NUMERIC LITERAL
+let december = 12;
+
+// BOOLEAN LITERAL
+let tired = true;
+//A value of truth or false
+
+// OBJECT LITERAL
+let soups = {
+    a: 'chicken noodle',
+    b: 'tomato',
+    c: 'beef and barley'
+};
+// objects hold what are known as key/value pairs. Our soup object has 3 keys (a, b, c), and each of those keys have their own value tied to it
+
+console.log(soup.c); //beef and barley
+/*
+    - objects use something called dot notation. Dot notation allows us to dig through an object and grab specific data
+    - above we are calling our 'soup' variable and using dot notation to grab the key 'c' that's inside our object. Doing so will return the value associated with the key of 'c' (beef and barley)
+*/
+
+// ARRAY LITERAL
+let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+console.log(days.toString()); //Monday, Tuesday, Wednesday, Thursday, Friday
+//this is a method that comes built into javascript to help us separate an array into strings
+
+console.log(days.length);
+/* this is a property of arrays. Properties are things that already belong to an object
+
+*/
